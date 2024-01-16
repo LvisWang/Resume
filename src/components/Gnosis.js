52e8ed6498cfae8dcf2982a44441ebc96f01716b
LvisWang <1583461966@qq.com>
2024-01-16 21:32:08 +0800

    typo

--- src/components/Gnosis.js
+++ src/components/Gnosis.js
@@ -4,7 +4,7 @@ import './styles.css';
 const Gnosis = () => {
     return (
         <div className='divWithPadding'>
-            <h3> Gnosis Safe | <a href="https://sixdegree.substack.com/p/user-engagement-analysis" target="_blank" rel="noopener noreferrer" style={{ color: '#004080', textDecoration: 'underline' }}>User Onchain Behaviour Report </a> |
+            <h3> Gnosis Safe | <a href="https://sixdegree.substack.com/p/user-engagement-analysis" target="_blank" rel="noopener noreferrer" style={{ color: '#004080', textDecoration: 'underline' }}>User Onchain Engagement Report </a> |
                  <a href="https://dune.com/queries/1266525" target="_blank" rel="noopener noreferrer" style={{ color: '#004080', textDecoration: 'underline' }}> Dashboard</a> | <a href="https://github.com/safe-global/safe-user-allocation-reports/issues/427#issuecomment-1251932785" target="_blank" rel="noopener noreferrer" style={{color: '#004080',  textDecoration: 'underline'  }}>Feedback</a>
             </h3> 
 
